263209dc469a75b4b348863e50749494097379da
Aziret Alisher <aziretalisher@gmail.com>
2023-05-04 12:32:22 +0100

    Add /profile/:id and /image endpoints

--- server.js
+++ server.js
@@ -52,6 +52,31 @@ app.post("/register", (req, res) => {
   res.json(database.users[database.users.length - 1]);
 });
 
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      res.json(user);
+    }
+  });
+  res.status(404).json("no such user");
+});
+
+app.post("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id == id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
