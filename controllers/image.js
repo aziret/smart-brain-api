f93b168fa67c5b0b25d2e6b9026b61b226415b39
Aziret Alisher <aziretalisher@gmail.com>
2023-05-05 18:19:47 +0100

    Add basic validation and add Clarifai API calls

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,59 @@
+require("dotenv").config();
+
+const PAT = process.env.PAT;
+const USER_ID = process.env.USER_ID;
+const APP_ID = process.env.APP_ID;
+
+const MODEL_ID = process.env.MODEL_ID;
+const MODEL_VERSION_ID = process.env.MODEL_VERSION_ID;
+
+const raw = (image_url) => {
+  return JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: image_url,
+          },
+        },
+      },
+    ],
+  });
+};
+
+const requestOptions = (image_url) => {
+  return {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Key " + PAT,
+    },
+    body: raw(image_url),
+  };
+};
+
+const handleApiCall = (req, res) => {
+  fetch(
+    "https://api.clarifai.com/v2/models/" +
+      MODEL_ID +
+      "/versions/" +
+      MODEL_VERSION_ID +
+      "/outputs",
+    requestOptions(req.body.input)
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json("unable to work with API");
+    });
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
 
@@ -13,4 +69,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage: handleImage,
+  handleApiCall: handleApiCall,
 };
