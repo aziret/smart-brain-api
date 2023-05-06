759d00b8e4803b6df808a4cd1139eb2d3fc5aa4a
Aziret Alisher <aziretalisher@gmail.com>
2023-05-06 14:32:42 +0100

    Allow unauthorized requests

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const profile = require("./controllers/profile");
 const image = require("./controllers/image");
 require("dotenv").config();
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: "pg",
   connection: {
