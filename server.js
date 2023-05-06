2ca8aad14e42892692d334a83d6c20b3266d409a
Aziret Alisher <aziretalisher@gmail.com>
2023-05-06 13:24:03 +0100

    Add prod configs for database

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ require("dotenv").config();
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DATABASE_URL,
+    ssl: true,
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
   },
 });
 
